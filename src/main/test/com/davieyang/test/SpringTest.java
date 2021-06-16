7ca7399486781c60dee281164eeae41387e18aa8
davieyangcool <15901281916@163.com>
2021-06-16 18:48:58 +0800

    create new version

---
+++ src/main/test/com/davieyang/test/SpringTest.java
@@ -0,0 +1,41 @@
+package com.davieyang.test;
+
+import org.testng.annotations.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author davieyang
+ * @date 2021/06/16
+ */
+@Service
+public class SpringTest {
+    /** @Service
+     * Spring会自动扫描@Service注解的类，并把这些类纳入进Spring容器中管理
+     * 也可以使用@Component注解
+     */
+
+    @Test
+    public void testSpring() {
+        /** ApplicationContext
+         *  ApplicationContext是Spring中较高级别的容器,用于加载配置文件中的bean,并将所有的Bean集中在一起当有请求的分配bean
+         *  ClassPathXmlApplicationContext:从类路径ClassPath中寻找指定的XML配置文件，找到并装载完成ApplicationContext的实例化工作
+         *  ApplicationContext cxt = new ClassPathXmlApplicationContext("applicationContext.xml");
+         *  String [] configs = {"bean1.xml","bean2.xml","bean3.xml"};
+         *  ApplicationContext cxt = new ClassPathXmlApplicationContext(configs)
+         *  FileSystemXmlApplicationContext:从指定的文件系统路径中寻找指定的XML配置文件，找到并装载完成ApplicationContext的实例化工作
+         *
+         */
+        ApplicationContext applicationContext =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+        SpringTest springTest = (SpringTest) applicationContext.getBean("springTest");
+        springTest.sayHello();
+
+    }
+
+    public void sayHello() {
+        System.out.println("hello ay");
+    }
+
+}
